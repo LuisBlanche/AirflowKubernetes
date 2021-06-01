5c3a6d23291d34a2f302e51bec3cc08d7e5d453a
Luis Blanche <lblanche@dataswati.com>
2021-06-01 11:30:35 +0200

    feat: add impute wth xcom

--- dags/AirflowMLDag.py
+++ dags/AirflowMLDag.py
@@ -1,9 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
-import pandas as pd
 from airflow import DAG
-from airflow.utils.dates import days_ago
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.utils.dates import days_ago
 from kubernetes.client import models as k8s
 
 default_args = {
@@ -11,45 +10,103 @@ default_args = {
     "start_date": days_ago(2),
     "retries": 0,
     "retry_delay": timedelta(minutes=1),
-} 
+}
+
 
 def get_volume_components(
     dag_id,
-    host_path="/home/dev/Luis/odsc/AirflowKubernetes/dataswati/data", # PUT YOU OWN PATH HERE 
+    host_path="/home/dev/Luis/odsc/AirflowKubernetes/dataswati/data",  # PUT YOU OWN PATH HERE
     container_path="/app/data",
     volume_name="hostpath-volume",
 ):
     volume_data = k8s.V1Volume(name=volume_name, host_path=k8s.V1HostPathVolumeSource(path=host_path, type="Directory"))
     volume_mount_data = k8s.V1VolumeMount(
-        mount_path=container_path, name=volume_name, read_only=False, sub_path=f"dag_{dag_id}",
+        mount_path=container_path,
+        name=volume_name,
+        read_only=False,
+        sub_path=f"dag_{dag_id}",
     )
     return volume_data, volume_mount_data
 
-dag_id = 'Aiflow_ML_k8s'
+
+dag_id = "Aiflow_ML_k8s"
 
 VOLUME_DATA, VOLUME_MOUNT_DATA = get_volume_components(dag_id=dag_id, container_path="/tmp")
 
 
-resources = k8s.V1ResourceRequirements(requests={"memory": "1Gi", "cpu": "1",}, limits={"cpu": 2,},)
+resources = k8s.V1ResourceRequirements(
+    requests={
+        "memory": "1Gi",
+        "cpu": "1",
+    },
+    limits={
+        "cpu": 2,
+    },
+)
 DATA_PATH = "/app/data"
 
 
-with DAG(dag_id=dag_id, default_args= default_args, schedule_interval=None, max_active_runs=1) as dag:
-    make_dataset = KubernetesPodOperator(task_id='make_dataset',
-                                         trigger_rule='all_success', 
-                                         namespace='default', 
-                                         image="dataswatidevops/odsc_python_airflow_k8s",
-                                         labels={"airflow": "operator"},
-                                         name="airflow-operator-" + str(dag_id) + "-task",
-                                         in_cluster=True,
-                                         resources=resources,
-                                         get_logs=True,
-                                         is_delete_operator_pod=True,
-                                         image_pull_policy="Always",
-                                         cmds=[f"python potability/data/make_dataset.py {DATA_PATH}"],
-                                         volumes=[VOLUME_DATA],
-                                         volume_mounts=[VOLUME_MOUNT_DATA],
-                                         dag=dag,
-                                         do_xcom_push=True
-                                         ) 
-    
+with DAG(dag_id=dag_id, default_args=default_args, schedule_interval=None, max_active_runs=1) as dag:
+    make_dataset = KubernetesPodOperator(
+        task_id="make_dataset",
+        trigger_rule="all_success",
+        namespace="default",
+        image="dataswatidevops/odsc_python_airflow_k8s",
+        labels={"airflow": "operator"},
+        name="airflow-operator-" + str(dag_id) + "-task",
+        in_cluster=True,
+        resources=resources,
+        get_logs=True,
+        is_delete_operator_pod=True,
+        image_pull_policy="Always",
+        cmds=[f"python potability/data/make_dataset.py {DATA_PATH}"],
+        volumes=[VOLUME_DATA],
+        volume_mounts=[VOLUME_MOUNT_DATA],
+        dag=dag,
+        do_xcom_push=True,
+    )
+
+    impute_train_data = KubernetesPodOperator(
+        task_id="make_dataset",
+        trigger_rule="all_success",
+        namespace="default",
+        image="dataswatidevops/odsc_python_airflow_k8s",
+        labels={"airflow": "operator"},
+        name="airflow-operator-" + str(dag_id) + "-task",
+        in_cluster=True,
+        resources=resources,
+        get_logs=True,
+        is_delete_operator_pod=True,
+        image_pull_policy="Always",
+        cmds=[
+            f"python potability/data/impute.py {{task_instance.xcom_pull(task_ids='make_dataset', key='return_values')['train_features_path]}} {DATA_PATH}/processed/train_features_imputed.csv"
+        ],
+        volumes=[VOLUME_DATA],
+        volume_mounts=[VOLUME_MOUNT_DATA],
+        dag=dag,
+        do_xcom_push=True,
+    )
+
+    impute_unseen_data = KubernetesPodOperator(
+        task_id="make_dataset",
+        trigger_rule="all_success",
+        namespace="default",
+        image="dataswatidevops/odsc_python_airflow_k8s",
+        labels={"airflow": "operator"},
+        name="airflow-operator-" + str(dag_id) + "-task",
+        in_cluster=True,
+        resources=resources,
+        get_logs=True,
+        is_delete_operator_pod=True,
+        image_pull_policy="Always",
+        cmds=[
+            f"python potability/data/impute.py {{task_instance.xcom_pull(task_ids='make_dataset', key='return_values')['unseen_features_path]}} {DATA_PATH}/processed/train_features_imputed.csv"
+        ],
+        volumes=[VOLUME_DATA],
+        volume_mounts=[VOLUME_MOUNT_DATA],
+        dag=dag,
+        do_xcom_push=True,
+    )
+
+
+make_dataset >> [impute_train_data, impute_unseen_data]
--- dataswati/potability/data/make_dataset.py
+++ dataswati/potability/data/make_dataset.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
+import json
 import logging
 from pathlib import Path
-import pandas as pd 
+
 import fire
+import pandas as pd
 
 
 def run(data_path_str: str, target_name: str = "Potability"):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info(f'Spliting unseen and training data in {data_path_str} / interim')
+    logger.info(f"Spliting unseen and training data in {data_path_str} / interim")
     data_path = Path(data_path_str)
 
     df = pd.read_csv(data_path / "raw" / "archive.zip")
@@ -20,10 +22,22 @@ def run(data_path_str: str, target_name: str = "Potability"):
     train.reset_index(drop=True, inplace=True)
     train_features, train_target = split_features_target(train, target_name)
     unseen_features, unseen_real_target = split_features_target(unseen, target_name)
-    train_features.to_csv(data_path / "interim" / "train_features.csv", index=False)
-    train_target.to_csv(data_path / "interim" / "train_target.csv", index=False)
-    unseen_features.to_csv(data_path / "interim" / "unseen_features.csv", index=False)
-    unseen_real_target.to_csv(data_path / "interim" / "unseen_real_target.csv", index=False)
+    train_features_path = data_path / "interim" / "train_features.csv"
+    train_features.to_csv(train_features_path, index=False)
+    train_target_path = data_path / "interim" / "train_target.csv"
+    train_target.to_csv(train_target_path, index=False)
+    unseen_features_path = data_path / "interim" / "unseen_features.csv"
+    unseen_features.to_csv(unseen_features_path, index=False)
+    unseen_real_target_path = data_path / "interim" / "unseen_real_target.csv"
+    unseen_real_target.to_csv(unseen_real_target_path, index=False)
+    xcom_return = {
+        "train_features_path": train_features_path,
+        "train_target_path": train_target_path,
+        "unseen_features_path": unseen_features_path,
+        "unseen_real_target": unseen_real_target,
+    }
+    with open("/airflow/xcom/return.json", "w") as file:
+        json.dump(xcom_return, file)
 
 
 def split_features_target(df, target_name):
@@ -31,7 +45,8 @@ def split_features_target(df, target_name):
     features = df.drop(columns=[target_name])
     return features, target
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
     fire.Fire(run)
