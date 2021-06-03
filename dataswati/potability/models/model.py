edf08796386e67f457894c2415cfb014d8ddbfba
Luis Blanche <lblanche@dataswati.com>
2021-06-03 12:31:17 +0200

    feat: improve model evaluation

--- dataswati/potability/models/model.py
+++ dataswati/potability/models/model.py
@@ -1,71 +1,99 @@
 import logging
+import os
 
+import joblib
+import matplotlib.pyplot as plt
 import numpy as np
 from interpret.glassbox import ExplainableBoostingClassifier
-from lightgbm import LightGBMClassifier
+from lightgbm import LGBMClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.inspection import permutation_importance
+from sklearn.metrics import accuracy_score, f1_score, plot_confusion_matrix
 from sklearn.model_selection import RandomizedSearchCV
 
 
 class PotabilityModel:
-    """Wrapper class that handles different types of ML model and random grid search training"""
-
     def __init__(self, model: str) -> None:
         if model == "interpret":
-            self.model = ExplainableBoostingClassifier()
+            self.model_type = "interpret"
+            self.initial_model = ExplainableBoostingClassifier()
         elif model == "rf":
-            self.model = RandomForestClassifier()
+            self.model_type = "rf"
+            self.initial_model = RandomForestClassifier()
         elif model == "lightgbm":
-            self.model = LightGBMClassifier()
+            self.model_type = "lightgbm"
+            self.initial_model = LGBMClassifier()
 
     def __repr__(self) -> str:
-        return f"This a {self.model} model"
+        return f"This a {self.model_type} model"
 
     def gridsearch(self, X, y, n_iter=100, n_jobs=1, cv=3):
-        """Operates a random grid search
-
-        Args:
-            X (pd.DataFrame): Features
-            y (pd.Series): target
-            n_iter (int, optional): number of randdom iteration. Defaults to 100.
-            n_jobs (int, optional): number of parallel jobs. Defaults to 1.
-            cv (int, optional): cross validation folds. Defaults to 3.
-        """
         random_grid = self._get_random_grid_search()
         rscv = RandomizedSearchCV(
-            estimator=self.model, param_distributions=random_grid, n_iter=n_iter, cv=cv, verbose=2, n_jobs=n_jobs
+            estimator=self.initial_model,
+            param_distributions=random_grid,
+            n_iter=n_iter,
+            cv=cv,
+            verbose=2,
+            n_jobs=n_jobs,
         )
-        rscv.fit(X, y)
-        logging.info(f"Best score for {self.model} : {rscv.best_score_} ")
+        rscv.fit(X, y.values.ravel())
+        logging.info(f"Best score for {self.model_type} : {rscv.best_score_} ")
         self.best_params = rscv.best_params_
         self.best_cv_model = rscv.best_estimator_
 
     def fit(self, X, y):
         self.best_cv_model.fit(X, y)
 
+    def save_best_model(self, model_path: str = "models"):
+        joblib.dump(self, f"{model_path}/{self.model_type}/potability.joblib")
+
     def predict(self, X):
-        self.best_cv_model.predict(X)
+        return self.best_cv_model.predict(X)
+
+    def evaluate(self, X_test, y_test, model_path: str = "models"):
+        y_pred = self.predict(X_test)
+        print(f"Model accuracy = {accuracy_score(y_test.values, y_pred)}")
+        print(f"Model F1 Score= {f1_score(y_test.values, y_pred)}")
+        plot_confusion_matrix(self.best_cv_model, X_test, y_test)
+        plt.savefig(os.path.join(model_path, self.model_type, "confusion_matrix.png"))
+        self.save_feature_importance(X_test, y_test, model_path)
+
+    def save_feature_importance(self, X_test, y_test, model_path: str = "models"):
+
+        result = permutation_importance(self.best_cv_model, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2)
+        sorted_idx = result.importances_mean.argsort()
+        fig, ax = plt.subplots()
+        ax.boxplot(result.importances[sorted_idx].T, vert=False, labels=X_test.columns[sorted_idx])
+        ax.set_title("Permutation Importances (test set)")
+        fig.tight_layout()
+        plt.savefig(os.path.join(model_path, self.model_type, "feature_importance.png"))
+        if self.model_type == "interpret":
+            ebm_global = self.best_cv_model.explain_global()
+            for i, feature in enumerate(X_test.columns):
+                with open(os.path.join(model_path, self.model_type, feature), "w") as f:
+                    ebm_global.visualize(i).write_html(os.path.join(model_path, self.model_type, f"{feature}.html"))
 
     def _get_random_grid_search(self):
         # RF
-        n_estimators = [int(x) for x in np.linspace(start=20, stop=150, num=10)]
+        n_estimators = [int(x) for x in np.linspace(start=10, stop=200, num=10)]
         max_features = ["auto", "sqrt"]
-        max_depth = [int(x) for x in np.linspace(4, 30, num=11)]
+        max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
         min_samples_split = [2, 5, 10]
         min_samples_leaf = [1, 2, 4]
         bootstrap = [True, False]
         # LightGBM
-        learning_rate = np.random.uniform(0, 1)
-        boosting_type = np.random.choice(["gbdt", "dart", "goss"])
-        sub_feature = np.random.uniform(0, 1)
+        learning_rate = [x for x in np.linspace(0.01, 1, num=10)]
+        boosting_type = ["gbdt", "dart", "goss"]
+        sub_feature = [x for x in np.linspace(0.01, 1, num=10)]
         num_leaves = [int(x) for x in np.linspace(20, 300, num=10)]
         min_data = [int(x) for x in np.linspace(10, 110, num=11)]
         # ExplainableBoosting
         max_bins = [int(x) for x in np.linspace(start=128, stop=1024, num=6)]
         binning = ["uniform", "quantile", "quantile_humanized"]
         interactions = [int(x) for x in np.linspace(10, 110, num=11)]
-        max_rounds = [int(x) for x in np.linspace(start=1000, stop=6000, num=10)]
-        if self.model == "rf":
+
+        if self.model_type == "rf":
             return {
                 "n_estimators": n_estimators,
                 "max_features": max_features,
@@ -74,7 +102,7 @@ class PotabilityModel:
                 "min_samples_leaf": min_samples_leaf,
                 "bootstrap": bootstrap,
             }
-        elif self.model == "lightgbm":
+        elif self.model_type == "lightgbm":
             return {
                 "learning_rate": learning_rate,
                 "boosting_type": boosting_type,
@@ -83,12 +111,12 @@ class PotabilityModel:
                 "min_data": min_data,
                 "max_depth": max_depth,
             }
-        elif self.model == "interpret":
+        elif self.model_type == "interpret":
             return {
                 "learning_rate": learning_rate,
                 "max_bins": max_bins,
                 "min_samples_leaf": min_samples_leaf,
                 "binning": binning,
                 "interactions": interactions,
-                "max_rounds": max_rounds,
+                "max_rounds": [int(x) for x in np.linspace(start=1000, stop=6000, num=10)],
             }
