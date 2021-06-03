e8e1b4245a023d057a63eb60258359835ea2d6cd
Luis Blanche <lblanche@dataswati.com>
2021-06-03 18:06:07 +0200

    fix: model path

--- dataswati/potability/models/predict_model.py
+++ dataswati/potability/models/predict_model.py
@@ -6,8 +6,8 @@ import joblib
 import pandas as pd
 
 
-def load_model(model_path: str, model_type: str):
-    model = joblib.load(os.path.join(model_path, model_type, "potability.joblib"))
+def load_model(model_path: str):
+    model = joblib.load(model_path)
     return model
 
 
@@ -16,14 +16,13 @@ def run(
     unseen_target_input_path: str,
     predictions_output_path: str,
     model_path: str,
-    model_type: str,
 ):
     logging.info(f"Reading unseen data from {unseen_features_input_path}")
     logging.info(f"Reading real target data from {unseen_target_input_path}")
 
     unseen_features = pd.read_csv(unseen_features_input_path)
     unseen_target = pd.read_csv(unseen_target_input_path)
-    model = load_model(model_path, model_type)
+    model = load_model(model_path)
     predictions = model.predict(unseen_features)
     pd.Series(predictions, name="Potability").to_csv(predictions_output_path, index=False)
     model.evaluate(unseen_features, unseen_target)
